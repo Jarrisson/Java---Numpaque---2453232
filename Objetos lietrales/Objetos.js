02b0eae6d598c25774e86d603a69e521a66c753f
Jarrisson <jarrisonumpaque2302@outlook.es>
2022-08-06 17:38:51 -0500

    se agrega trabajos de objetos

--- Objetos lietrales/Objetos.js
+++ Objetos lietrales/Objetos.js
@@ -1,7 +1,34 @@
-let aritmetica = {
-    numerador1: 2,
-    numerador2: 3,
-    denominador1: 5,
-    denominador2: 9,
+let fraccion = {
+    numerador1: 20,
+    numerador2: 5,
+    denominador1: 8,
+    denominador2: 8,
 };
-console.log()
+console.log(fraccion)
+    //suma de fracciones
+if (fraccion.denominador1 == fraccion.denominador2) {
+    suma = fraccion.numerador1 + fraccion.numerador2
+    console.log(`suma total: ${suma}/${fraccion.denominador1}`)
+} else {
+    let pc1 = fraccion.numerador1 * fraccion.denominador2
+    let pc2 = fraccion.denominador1 * fraccion.numerador2
+    let pc3 = fraccion.denominador1 * fraccion.denominador2
+    let total = pc1 += pc2
+    console.log(`total-suma: ${total}/${pc3}`)
+}
+//resta
+if (fraccion.denominador1 == fraccion.denominador2) {
+    suma = fraccion.numerador1 - fraccion.numerador2
+    console.log(`Resta total: ${suma}/${fraccion.denominador1}`)
+} else {
+    let pc1 = fraccion.numerador1 * fraccion.denominador2
+    let pc2 = fraccion.denominador1 * fraccion.numerador2
+    let pc3 = fraccion.denominador1 * fraccion.denominador2
+    let total = pc1 -= pc2
+    console.log(`total-resta: ${total}/${pc3}`)
+}
+// Multiplicacion
+pc1 = fraccion.numerador1 * fraccion.numerador2
+pc2 = fraccion.denominador1 * fraccion.denominador2
+console.log(`total-multi: ${pc1}/${pc2}`)
+    //Division
